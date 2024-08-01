b8d74bb70ffbfc606e65920b6279a9812e825942
Sododa <sophieododa@gmail.com>
2024-08-01 13:34:24 +0300

    lockbox

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -1,15 +1,27 @@
 #!/usr/bin/python3
-"""task 0"""
+""" task 0
+"""
 
 
 def canUnlockAll(boxes):
-    """prototype"""
+    """Function that determines if all the boxes are op"""
+    # Initialize set of visited boxes, starting with the first box
     visited = {0}
+    # Initialize queue with the first box
     queue = [boxes[0]]
+    # While there are boxes in the queue
+    # print("boxes type is {}".format(type(boxes)))
     while queue:
+        # Take the first box from the queue
         box = queue.pop(0)
+        # Iterate through the keys in the box
         for key in box:
+            # If the key corresponds to a locked box that has not been
+            # visited yet
             if key not in visited and key < len(boxes):
+                # Mark the box as visited
                 visited.add(key)
+                # Add the box to the queue to explore its keys
                 queue.append(boxes[key])
-                return len(visited) == len(boxes)
+    # Return whether all boxes have been visited
+    return len(visited) == len(boxes)
