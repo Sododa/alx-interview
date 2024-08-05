c2e250de371019dc3a45ee5b06e8dba7ec017ee9
Sododa <sophieododa@gmail.com>
2024-08-05 10:12:27 +0300

    minimum

---
+++ 0x02-minimum_operations/0-minoperations.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""task 0"""
+
+
+
+def minOperations(n):
+    """Prototype: def minOperations(n)"""
+    if not isinstance(n, int):
+        return 0
+    ops_count = 0
+    clipboard = 0
+    done = 1
+    while done < n:
+        if clipboard == 0:
+            clipboard = done
+            done += clipboard
+            ops_count += 2
+        elif n - done > 0 and (n - done) % done == 0:
+            clipboard = done
+            done += clipboard
+            ops_count += 2
+        elif clipboard > 0:
+            done += clipboard
+            ops_count += 1
+    return ops_count
